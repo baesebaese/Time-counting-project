c2451d733410151e1a96e577f0d8c89b85cbd3f5
yomi <misokkin@gmail.com>
2023-10-29 17:30:30 +0900

    Initial commit

---
+++ src/main/java/com/example/tenthousandhoursrule/TenThousandHoursRuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tenthousandhoursrule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenThousandHoursRuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TenThousandHoursRuleApplication.class, args);
+    }
+
+}
