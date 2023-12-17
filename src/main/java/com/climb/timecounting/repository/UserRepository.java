7e442b1dad6e9e42e76d834dac10443e4b9f3059
morendo <morendo90@gmail.com>
2023-12-17 23:53:56 +0900

    #13 - Querydsl 지원을 이용하여 api 검색 옵션 추가.
    `QuerydslPredicateExecutor`,
    `QuerydslBinderCustomizer` 를 이용하여 간단하게 개발.
    검색이 필요한지는 기획확인 필요.

--- src/main/java/com/climb/timecounting/repository/UserRepository.java
+++ src/main/java/com/climb/timecounting/repository/UserRepository.java
@@ -1,9 +1,26 @@
 package com.climb.timecounting.repository;
 
+import com.climb.timecounting.domain.Quser;
 import com.climb.timecounting.domain.user;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface UserRepository extends JpaRepository<user, String> {
+public interface UserRepository extends
+        JpaRepository<user, String>
+    , QuerydslPredicateExecutor<user>
+    , QuerydslBinderCustomizer<Quser>
+{
+
+    @Override
+    default void customize(QuerydslBindings bindings, Quser root) {
+
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.user_id);
+    }
 }
