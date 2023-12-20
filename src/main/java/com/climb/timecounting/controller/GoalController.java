d00f670d501468a7fab143884ca208267b2983f2
morendo <morendo90@gmail.com>
2023-12-20 22:00:30 +0900

    #13 - Controller 추가
    컨트롤러 추가.
    view test 작성. 기능 구현 중인 유닛 테스트는 '@Disabled' 처리

---
+++ src/main/java/com/climb/timecounting/controller/GoalController.java
@@ -0,0 +1,9 @@
+package com.climb.timecounting.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/goals")
+@Controller
+public class GoalController {
+}
